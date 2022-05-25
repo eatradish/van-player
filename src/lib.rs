7f221bd4302bae34b31ae89969400e0a70868740
eatradish <sakiiily@aosc.io>
2022-05-25 12:07:01 +0800

    mpv: add function to get current media info

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod mpv;
+
+pub use mpv::add;
+pub use mpv::play;
--- src/mpv.rs
+++ src/mpv.rs
@@ -4,10 +4,20 @@ use std::{
     collections::HashMap,
     sync::{
         atomic::{AtomicUsize, Ordering},
-        mpsc::Receiver,
+        mpsc::{Receiver, Sender},
         Mutex,
     },
+    time::Duration,
 };
+
+use anyhow::{anyhow, bail, Result};
+
+#[derive(Debug)]
+struct MediaInfo {
+    title: String,
+    duration: f64,
+    current_time: f64,
+}
 
 const DEFAULT_VOL: f64 = 50.0;
 
@@ -18,13 +28,29 @@ lazy_static! {
     static ref CURRENT: AtomicUsize = AtomicUsize::new(0);
 }
 
-fn add(url: &'static str) {
-    let mut queue = QUEUE.lock().unwrap();
-    queue.push((url, FileState::AppendPlay, None));
+macro_rules! check_err {
+    ($i:expr, $err_tx:expr) => {
+        if let Err(e) = $i {
+            $err_tx.send(e.to_string()).unwrap();
+            return;
+        }
+    };
 }
 
-fn play(vol_rx: Receiver<f64>, next_rx: Receiver<bool>, prev_rx: Receiver<bool>) -> Result<()> {
-    play_inner(vol_rx, next_rx, prev_rx);
+pub fn add(url: &'static str) -> Result<()> {
+    let mut queue = QUEUE.lock().map_err(|e| anyhow!("{}", e))?;
+    queue.push((url, FileState::AppendPlay, None));
+
+    Ok(())
+}
+
+pub fn play(
+    vol_rx: Receiver<f64>,
+    next_rx: Receiver<bool>,
+    prev_rx: Receiver<bool>,
+    get_info_tx: Sender<bool>,
+) -> Result<()> {
+    play_inner(vol_rx, next_rx, prev_rx, get_info_tx)?;
 
     Ok(())
 }
@@ -44,49 +70,65 @@ fn seekable_ranges(demuxer_cache_state: &MpvNode) -> Option<Vec<(f64, f64)>> {
     Some(res)
 }
 
-fn play_inner(vol_rx: Receiver<f64>, next_rx: Receiver<bool>, prev_rx: Receiver<bool>) {
+fn play_inner(
+    vol_rx: Receiver<f64>,
+    next_rx: Receiver<bool>,
+    prev_rx: Receiver<bool>,
+    get_info_tx: Sender<bool>,
+) -> Result<()> {
     let mut ev_ctx = MPV.create_event_context();
-    ev_ctx.observe_property("volume", Format::Int64, 0).unwrap();
+    ev_ctx
+        .observe_property("volume", Format::Int64, 0)
+        .map_err(|e| anyhow!("{}", e))?;
     ev_ctx
         .observe_property("demuxer-cache-state", Format::Node, 0)
-        .unwrap();
+        .map_err(|e| anyhow!("{}", e))?;
+    let (err_tx, err_rx) = std::sync::mpsc::channel();
+    let err_tx_2 = err_tx.clone();
+    let err_tx_3 = err_tx.clone();
     crossbeam::scope(|scope| {
         scope.spawn(move |_| {
-            MPV.set_property("volume", DEFAULT_VOL).unwrap();
-            MPV.set_property("vo", "null").unwrap();
+            check_err!(MPV.set_property("volume", DEFAULT_VOL), err_tx);
+            check_err!(MPV.set_property("vo", "null"), err_tx);
             let current = CURRENT.load(Ordering::SeqCst);
-            MPV.playlist_load_files(&QUEUE.lock().unwrap()[current..])
-                .unwrap();
+            check_err!(
+                MPV.playlist_load_files(&QUEUE.lock().unwrap()[current..]),
+                err_tx
+            );
         });
         scope.spawn(move |_| loop {
             let mut current = CURRENT.load(Ordering::SeqCst);
             if let Ok(v) = vol_rx.try_recv() {
                 println!("vol is set to {}", v);
-                MPV.set_property("volume", v).unwrap();
+                check_err!(MPV.set_property("volume", v), err_tx_2);
             }
             if let Ok(next) = next_rx.try_recv() {
-                let queue = QUEUE.lock().unwrap();
+                let queue = QUEUE.lock();
+                check_err!(queue, err_tx_2);
+                let queue = queue.unwrap();
                 if next {
                     if current == queue.len() - 1 {
-                        MPV.playlist_next_force().unwrap();
-                        MPV.playlist_load_files(&queue[current..]).unwrap();
+                        check_err!(MPV.playlist_next_force(), err_tx_2);
+                        check_err!(MPV.playlist_load_files(&queue[current..]), err_tx_2);
                         continue;
                     } else {
-                        MPV.playlist_next_force().unwrap();
+                        check_err!(MPV.playlist_next_force(), err_tx_2);
                     }
                 }
             }
             if let Ok(prev) = prev_rx.try_recv() {
-                let queue = QUEUE.lock().unwrap();
+                let queue = QUEUE.lock();
+                check_err!(queue, err_tx_2);
+                let queue = queue.unwrap();
                 if prev {
                     println!("prev!");
                     if current == 0 {
                         current = queue.len() - 1;
-                        MPV.playlist_previous_force().unwrap();
-                        MPV.playlist_load_files(&queue[current..]).unwrap();
+                        check_err!(MPV.playlist_previous_force(), err_tx_2);
+                        check_err!(MPV.playlist_load_files(&queue[current..]), err_tx_2);
                         continue;
                     } else {
-                        MPV.playlist_previous_force().unwrap();
+                        check_err!(MPV.playlist_previous_force(), err_tx_2);
                     }
                 }
             }
@@ -98,8 +140,10 @@ fn play_inner(vol_rx: Receiver<f64>, next_rx: Receiver<bool>, prev_rx: Receiver<
                 Ok(Event::EndFile(r)) => {
                     println!("next!");
                     let current = CURRENT.load(Ordering::SeqCst);
-                    let queue_len = QUEUE.lock().unwrap().len();
-                    if current < queue_len - 1 {
+                    let queue = QUEUE.lock();
+                    check_err!(queue, err_tx_3);
+                    let queue = queue.unwrap();
+                    if current < queue.len() - 1 {
                         CURRENT.store(current + 1, Ordering::SeqCst);
                     }
                     println!("Exiting! Reason: {:?}", r);
@@ -111,12 +155,16 @@ fn play_inner(vol_rx: Receiver<f64>, next_rx: Receiver<bool>, prev_rx: Receiver<
                     change: PropertyData::Node(mpv_node),
                     ..
                 }) => {
-                    let ranges = seekable_ranges(mpv_node).unwrap();
-                    println!("Seekable ranges updated: {:?}", ranges);
+                    if let Some(ranges) = seekable_ranges(mpv_node) {
+                        println!("Seekable ranges updated: {:?}", ranges);
+                    }
+                    get_info_tx.send(true).ok();
                 }
                 Ok(Event::Deprecated(_)) => {
-                    let queue = QUEUE.lock().unwrap();
-                    MPV.playlist_load_files(&queue).unwrap();
+                    let queue = QUEUE.lock();
+                    check_err!(queue, err_tx_3);
+                    let queue = queue.unwrap();
+                    check_err!(MPV.playlist_load_files(&queue), err_tx_3);
                     CURRENT.store(0, Ordering::SeqCst);
                 }
                 Ok(e) => println!("Event triggered: {:?}", e),
@@ -124,23 +172,56 @@ fn play_inner(vol_rx: Receiver<f64>, next_rx: Receiver<bool>, prev_rx: Receiver<
             }
         });
     })
-    .unwrap();
+    .map_err(|e| anyhow!("{:?}", e))?;
+
+    if let Ok(e) = err_rx.recv() {
+        return Err(anyhow!("{}", e));
+    }
+
+    Ok(())
+}
+
+fn get_current_media_info(get_info_rx: Receiver<bool>) -> Result<MediaInfo> {
+    if let Ok(rx) = get_info_rx.recv_timeout(Duration::from_secs(60)) {
+        if rx {
+            let title = MPV
+                .get_property::<String>("media-title")
+                .map_err(|e| anyhow!("{}", e))?;
+            let duration = MPV
+                .get_property::<f64>("duration")
+                .map_err(|e| anyhow!("{}", e))?;
+            let current_time = MPV
+                .get_property::<f64>("time-pos")
+                .map_err(|e| anyhow!("{}", e))?;
+
+            return Ok(MediaInfo {
+                title,
+                duration,
+                current_time,
+            });
+        }
+    }
+
+    bail!("Mpv playlist is empty!")
 }
 
 #[test]
 fn test_play() {
-    use std::time::Duration;
     let (tx, rx) = std::sync::mpsc::channel();
     let (next_tx, next_rx) = std::sync::mpsc::channel();
     let (prev_tx, prev_rx) = std::sync::mpsc::channel();
-    add("https://www.bilibili.com/video/BV1GR4y1w7CR");
-    add("https://www.bilibili.com/video/BV133411V7dY");
-    add("https://www.bilibili.com/video/BV1WL4y1F7Uj");
-    add("https://www.bilibili.com/video/BV18B4y127QA");
+    let (get_info_tx, get_info_rx) = std::sync::mpsc::channel();
+    add("https://www.bilibili.com/video/BV1gr4y1b7vN").unwrap();
+    add("https://www.bilibili.com/video/BV1GR4y1w7CR").unwrap();
+    add("https://www.bilibili.com/video/BV133411V7dY").unwrap();
+    add("https://www.bilibili.com/video/BV1WL4y1F7Uj").unwrap();
+    add("https://www.bilibili.com/video/BV18B4y127QA").unwrap();
     let work = std::thread::spawn(|| {
-        play(rx, next_rx, prev_rx).unwrap();
+        play(rx, next_rx, prev_rx, get_info_tx).unwrap();
     });
     tx.send(100.0).unwrap();
+    std::thread::sleep(Duration::from_secs(10));
+    dbg!(get_current_media_info(get_info_rx).unwrap());
     std::thread::sleep(Duration::from_secs(10));
     next_tx.send(true).unwrap();
     std::thread::sleep(Duration::from_secs(10));
