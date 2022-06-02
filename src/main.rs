a5aecbae7593a55e0e1e7b8d6d3b030b7bba2102
eatradish <sakiiily@aosc.io>
2022-06-02 16:33:06 +0800

    main: display log

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use cursive::{Cursive, CursiveExt};
+use cursive::{Cursive, CursiveExt, reexports::log};
+use van_core::destroy_mpv;
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
@@ -9,14 +10,21 @@ struct Args {
 }
 
 fn main() {
+    cursive::logger::init();
+    log::set_max_level(log::LevelFilter::Info);
     let mut siv = Cursive::default();
 
     let (control_tx, control_rx) = std::sync::mpsc::channel();
+    let control_tx_clone = control_tx.clone();
     let args = Args::parse().args;
     if let Err(e) = van_core::init_siv(&mut siv, args, control_tx, control_rx) {
         eprintln!("{}", e);
         std::process::exit(1);
     }
+
+    siv.add_global_callback('q', move |_| {
+        destroy_mpv(control_tx_clone.clone()).unwrap();
+    });
 
     siv.run();
 }
