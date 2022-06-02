5532b41622b0809c7021c3cf079ea0f8f001e338
eatradish <sakiiily@aosc.io>
2022-06-02 08:42:00 +0800

    meta: update doc

--- van-core/src/lib.rs
+++ van-core/src/lib.rs
@@ -36,7 +36,7 @@ struct CurrentStatus {
 ///
 /// let mut siv = Cursive::default();
 ///
-/// if let Err(e) = van_core::init_siv(&mut siv, vec!["https://www.bilibili.com/video/BV1HB4y1175c"]) {
+/// if let Err(e) = van_core::init_siv(&mut siv, vec!["https://www.bilibili.com/video/BV1HB4y1175c".to_string()]) {
 ///     eprintln!("{}", e);
 ///     std::process::exit(1);
 /// }
